42f849241e9354f82145d3d6feb02581125e752a
EugeniGryniuk <125498243+EugeniGryniuk@users.noreply.github.com>
2023-03-23 16:08:05 +0100

    BasicJava HomeWork 20.03.2023
    BasicJava HomeWork 20.03.2023

---
+++ T-R20.03.2023/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Planet saturn = new Planet("Сатурн");
+        Planet earth = new Planet("Земля", 1);
+        Planet mars = new Planet("Марс",2);
+        Planet neptune = new Planet("Нептун", 14);
+        Planet uranus = new Planet("Уран");
+
+    }
+}
