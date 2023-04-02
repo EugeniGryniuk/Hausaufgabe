bd7370aac690e0cdaa196980f1df0e1e9b1f59df
EugeniGryniuk <125498243+EugeniGryniuk@users.noreply.github.com>
2023-04-02 15:40:03 +0200

    BasicJava homework 29.03.2023
    BasicJava homework 29.03.2023

---
+++ T-R29.03.2023/src/Number1/Fruit.java
@@ -0,0 +1,13 @@
+package Number1;
+
+public class Fruit {
+    String name;
+    boolean isTasty;
+    String color;
+
+    Fruit(String name, boolean isTasty, String color) {
+        this.name = name;
+        this.isTasty = isTasty;
+        this.color = color;
+    }
+}
